87626a9b8257f6629b49a75db418f6b667b27956
luc <luc.leonardi@opsonet.com>
2023-08-18 09:55:39 +0200

    test OK more than 80%

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,8 +2,6 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
 describe("Given I am connected as an employee", () => {
@@ -81,6 +79,95 @@ describe("Given I am connected as an employee", () => {
       })
       console.log('createMock.mock.calls[0][0].data', createMock.mock.calls[0][0].data)
       expect(createMock.mock.calls[0][0].data.get("email")).toEqual("user@email.com")
+    })
+  })
+  describe('when submit new Bill', () => {
+    test('then call update method on store', () => {
+
+
+      const mockGetElementById = jest.fn()
+
+      mockGetElementById.mockReturnValue({})
+      localStorage.setItem("user", '{"email" : "user@email.com"}')
+      const createMock = jest.fn()
+      const goodFormatFile = new File(['img'], 'image.png', { type: 'image/png' })
+      const documentMock ={
+        querySelector: (s) => {
+          if (s === 'input[data-testid="file"]') {
+            return {
+              files: [goodFormatFile],
+              addEventListener: () => true,
+            }
+          } else {
+            return { addEventListener: () => true }
+          }
+        },
+        getElementById: mockGetElementById
+      }
+      const mockUpdate = jest.fn();
+      mockUpdate.mockResolvedValue({})
+      const storeMock ={
+        bills: () => {
+          return {
+            update: mockUpdate
+          }
+        }
+      }
+      const objInstance = new NewBill({
+        document: documentMock,
+        onNavigate: () => {},
+        store: storeMock,
+        localStorage: {}
+      });
+
+      objInstance.handleSubmit({ 
+        preventDefault: () => true ,
+        target: {
+          querySelector: (selector) => {
+            switch(selector) {
+              case 'select[data-testid="expense-type"]':
+                return {value: 'typedefrais'}
+                break;
+              case 'input[data-testid="expense-name"]':
+                return {value: 'nom'}
+                break;
+              case 'input[data-testid="amount"]':
+                return {value: '12'};
+                break;
+              case 'input[data-testid="datepicker"]':
+                return {value: 'date'};
+                break;
+              case 'input[data-testid="vat"]':
+                return {value: 'tva'};
+                break;
+              case 'input[data-testid="pct"]':
+                return {value: '34'};
+                break;
+              case 'textarea[data-testid="commentary"]':
+                return {value: 'commentaire'}
+                break;
+            }
+          }
+        }
+      })
+
+      const dataToCheck = {
+          email: 'user@email.com',
+          type: 'typedefrais',
+          name:  'nom',
+          amount: 12,
+          date:  'date',
+          vat: 'tva',
+          pct: 34,
+          commentary: 'commentaire',
+          fileUrl: null,
+          fileName: null,
+          status: 'pending'
+      }
+      const data = JSON.parse(mockUpdate.mock.calls[0][0].data);
+      console.log('data?', data);
+
+      expect(data).toMatchObject(dataToCheck)
     })
   })
   
